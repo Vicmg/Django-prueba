b3e354d262f56f9ab7f4cbf50220feec40efc399
Victor Martinez <vomartinez@misena.edu.co>
2021-04-01 11:16:35 -0500

    commit

--- empleado/urls.py
+++ empleado/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('admin/',admin.site.urls),  
     # Incluimos las urls de la app departamento 
     re_path('', include('applications.departamento.urls')),
-    re_path('', include('applications.persona.urls')),
     re_path('', include('applications.home.urls')),
+    re_path('', include('applications.persona.urls')),
     
     
 ]
