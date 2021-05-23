6bb410adfac4022b2eac3f7f7ae4929ac2835eb2
Victor Martinez <vomartinez@misena.edu.co>
2021-05-22 20:01:30 -0500

    widgets

--- applications/home/forms.py
+++ applications/home/forms.py
@@ -1,4 +1,5 @@
-from django import forms# se importa el form
+from django import forms
+from django.forms import widgets# se importa el form
 
 from .models import Prueba # se importa el modelo que tiene los campos
 
@@ -11,10 +12,22 @@ class PruebaForm(forms.ModelForm): #crea la clase del form
             "subtitulo",
             "cantidad",
         )
+        # widgets{
+        #     'titulo': forms.TextInput(
+        #         attrs={
+        #             'placeholder':'Ingrese texto aqui',
+        #         }
+        #     )
+        # }
+
     def clean_cantidad(self): # se crea el metodo  que valida los campos
-        cantidad = self.cleaned_data["cantidad"]
+
+        cantidad = self.cleaned_data['cantidad']# recupera el valor de la varaible 'cantidad' en el html
         if cantidad < 10:# segenera una condicion para el campo
             raise forms.ValidationError("Ingrese un valor mayor de 10")# y el mensaje dado el error
 
         return cantidad
+
+#personalizacion de formulario con widget
+    
 
